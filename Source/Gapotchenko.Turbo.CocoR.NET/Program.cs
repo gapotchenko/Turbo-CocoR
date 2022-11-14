b966a774a13697ddb1ba79cb1f91bdc3041b1272
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-11-15 00:01:50 +0100

    Refactoring.

--- Source/Gapotchenko.Turbo.CocoR.NET/Program.cs
+++ Source/Gapotchenko.Turbo.CocoR.NET/Program.cs
@@ -14,96 +14,95 @@
   in the attributed grammar or as a command-line option
   -------------------------------------------------------------------------*/
 
-using System;
-using System.IO;
+using Gapotchenko.Turbo.CocoR.NET.Grammar;
 
-namespace at.jku.ssw.Coco
+namespace Gapotchenko.Turbo.CocoR.NET;
+
+static class Program
 {
-    public class Coco
+    public static int Main(string[] arg)
     {
-
-        public static int Main(string[] arg)
+        Console.WriteLine("Turbo Coco/R .NET 2022.1 (Release build 2022.1.1)");
+        string srcName = null, nsName = null, frameDir = null, ddtString = null,
+        traceFileName = null, outDir = null;
+        bool emitLines = false;
+        int retVal = 1;
+        for (int i = 0; i < arg.Length; i++)
         {
-            Console.WriteLine("Coco/R (Apr 19, 2011)");
-            string srcName = null, nsName = null, frameDir = null, ddtString = null,
-            traceFileName = null, outDir = null;
-            bool emitLines = false;
-            int retVal = 1;
-            for (int i = 0; i < arg.Length; i++)
-            {
-                if (arg[i] == "-namespace" && i < arg.Length - 1) nsName = arg[++i].Trim();
-                else if (arg[i] == "-frames" && i < arg.Length - 1) frameDir = arg[++i].Trim();
-                else if (arg[i] == "-trace" && i < arg.Length - 1) ddtString = arg[++i].Trim();
-                else if (arg[i] == "-o" && i < arg.Length - 1) outDir = arg[++i].Trim();
-                else if (arg[i] == "-lines") emitLines = true;
-                else srcName = arg[i];
-            }
-            if (arg.Length > 0 && srcName != null)
-            {
-                try
-                {
-                    string srcDir = Path.GetDirectoryName(srcName);
-
-                    Scanner scanner = new Scanner(srcName);
-                    Parser parser = new Parser(scanner);
-
-                    traceFileName = Path.Combine(srcDir, "trace.txt");
-                    parser.trace = new StreamWriter(new FileStream(traceFileName, FileMode.Create));
-                    parser.tab = new Tab(parser);
-                    parser.dfa = new DFA(parser);
-                    parser.pgen = new ParserGen(parser);
-
-                    parser.tab.srcName = srcName;
-                    parser.tab.srcDir = srcDir;
-                    parser.tab.nsName = nsName;
-                    parser.tab.frameDir = frameDir;
-                    parser.tab.outDir = (outDir != null) ? outDir : srcDir;
-                    parser.tab.emitLines = emitLines;
-                    if (ddtString != null) parser.tab.SetDDT(ddtString);
-
-                    parser.Parse();
-
-                    parser.trace.Close();
-                    FileInfo f = new FileInfo(traceFileName);
-                    if (f.Length == 0) f.Delete();
-                    else Console.WriteLine("trace output is in " + traceFileName);
-                    Console.WriteLine("{0} errors detected", parser.errors.count);
-                    if (parser.errors.count == 0) { retVal = 0; }
-                }
-                catch (IOException)
-                {
-                    Console.WriteLine("-- could not open " + traceFileName);
-                }
-                catch (FatalError e)
-                {
-                    Console.WriteLine("-- " + e.Message);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Usage: Coco Grammar.ATG {{Option}}{0}"
-                                  + "Options:{0}"
-                                  + "  -namespace <namespaceName>{0}"
-                                  + "  -frames    <frameFilesDirectory>{0}"
-                                  + "  -trace     <traceString>{0}"
-                                  + "  -o         <outputDirectory>{0}"
-                                  + "  -lines{0}"
-                                  + "Valid characters in the trace string:{0}"
-                                  + "  A  trace automaton{0}"
-                                  + "  F  list first/follow sets{0}"
-                                  + "  G  print syntax graph{0}"
-                                  + "  I  trace computation of first sets{0}"
-                                  + "  J  list ANY and SYNC sets{0}"
-                                  + "  P  print statistics{0}"
-                                  + "  S  list symbol table{0}"
-                                  + "  X  list cross reference table{0}"
-                                  + "Scanner.frame and Parser.frame files needed in ATG directory{0}"
-                                  + "or in a directory specified in the -frames option.",
-                                  Environment.NewLine);
-            }
-            return retVal;
+            if (arg[i] == "-namespace" && i < arg.Length - 1) nsName = arg[++i].Trim();
+            else if (arg[i] == "-frames" && i < arg.Length - 1) frameDir = arg[++i].Trim();
+            else if (arg[i] == "-trace" && i < arg.Length - 1) ddtString = arg[++i].Trim();
+            else if (arg[i] == "-o" && i < arg.Length - 1) outDir = arg[++i].Trim();
+            else if (arg[i] == "-lines") emitLines = true;
+            else srcName = arg[i];
         }
+        if (arg.Length > 0 && srcName != null)
+        {
+            try
+            {
+                string srcDir = Path.GetDirectoryName(srcName);
 
-    } // end Coco
+                Scanner scanner = new Scanner(srcName);
+                Parser parser = new Parser(scanner);
 
-} // end namespace
+                traceFileName = Path.Combine(srcDir, "trace.txt");
+                parser.trace = new StreamWriter(new FileStream(traceFileName, FileMode.Create));
+                parser.tab = new Tab(parser);
+                parser.dfa = new DFA(parser);
+                parser.pgen = new ParserGen(parser);
+
+                parser.tab.srcName = srcName;
+                parser.tab.srcDir = srcDir;
+                parser.tab.nsName = nsName;
+                parser.tab.frameDir = frameDir;
+                parser.tab.outDir = outDir != null ? outDir : srcDir;
+                parser.tab.emitLines = emitLines;
+                if (ddtString != null) parser.tab.SetDDT(ddtString);
+
+                parser.Parse();
+
+                parser.trace.Close();
+                FileInfo f = new FileInfo(traceFileName);
+                if (f.Length == 0) f.Delete();
+                else Console.WriteLine("trace output is in " + traceFileName);
+                Console.WriteLine("{0} errors detected", parser.errors.count);
+                if (parser.errors.count == 0) { retVal = 0; }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("-- could not open " + traceFileName);
+            }
+            catch (FatalError e)
+            {
+                Console.WriteLine("-- " + e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Usage: turbo-coco-dotnet grammar.atg <option>{0}"
+                              + "{0}"
+                              + "Options:{0}"
+                              + "  -namespace arg  Namespace name.{0}"
+                              + "  -frames arg     Frame files directory.{0}"
+                              + "  -trace arg      Trace string (see below).{0}"
+                              + "  -o arg          Output directory.{0}"
+                              + "  -lines{0}"
+                              + "{0}"
+                              + "Valid characters in the trace string:{0}"
+                              + "  A  trace automaton{0}"
+                              + "  F  list first/follow sets{0}"
+                              + "  G  print syntax graph{0}"
+                              + "  I  trace computation of first sets{0}"
+                              + "  J  list ANY and SYNC sets{0}"
+                              + "  P  print statistics{0}"
+                              + "  S  list symbol table{0}"
+                              + "  X  list cross reference table{0}"
+                              + "{0}"
+                              + "\"Scanner.frame\" and \"Parser.frame\" files needed in ATG directory{0}"
+                              + "or in a directory specified by the -frames option.",
+                              Environment.NewLine);
+        }
+        return retVal;
+    }
+}
