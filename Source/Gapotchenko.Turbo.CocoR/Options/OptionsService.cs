17fba32de544a3f900b3bc3e72b8ed1bfade37b9
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-11-17 03:43:12 +0100

    In progress.

--- Source/Gapotchenko.Turbo.CocoR/Options/OptionsService.cs
+++ Source/Gapotchenko.Turbo.CocoR/Options/OptionsService.cs
@@ -4,27 +4,11 @@
 
 namespace Gapotchenko.Turbo.CocoR.Options;
 
-/*-------------------------------------------------------------------------
-  Trace output options
-  0 | A: prints the states of the scanner automaton
-  1 | F: prints the First and Follow sets of all nonterminals
-  2 | G: prints the syntax graph of the productions
-  3 | I: traces the computation of the First sets
-  4 | J: prints the sets associated with ANYs and synchronisation sets
-  6 | S: prints the symbol table (terminals, nonterminals, pragmas)
-  7 | X: prints a cross reference list of all syntax symbols
-  8 | P: prints statistics about the Coco run
-  
-  Trace output can be switched on by the pragma
-    $ { digit | letter }
-  in the attributed grammar or as a command-line option
-  -------------------------------------------------------------------------*/
-
 sealed class OptionsService : IOptionsService
 {
     public OptionsService(IReadOnlyList<string> args)
     {
-        // Keeps the automatic compatibility with prior Coco/R versions for .NET.
+        // Keeps the compatibility with prior Coco/R versions for .NET circa 2011.
 
         int argc = args.Count;
         for (int i = 0; i < argc; i++)
@@ -32,11 +16,11 @@ sealed class OptionsService : IOptionsService
             if (args[i] is "--namespace" or "-namespace" && i < argc - 1)
                 Namespace = Empty.Nullify(args[++i].Trim());
             else if (args[i] is "--frames" or "-frames" && i < argc - 1)
-                FramesDirectoryName = Empty.Nullify(args[++i].Trim());
+                FramesDirectoryName = Empty.Nullify(args[++i]);
             else if (args[i] is "--trace" or "-trace" && i < argc - 1)
                 Trace = Empty.Nullify(args[++i].Trim());
             else if (args[i] is "-o" or "--output" && i < argc - 1)
-                m_OutputDirectoryName = Empty.Nullify(args[++i].Trim());
+                m_OutputDirectoryName = Empty.Nullify(args[++i]);
             else if (args[i] is "--lines" or "-lines")
                 EmitLines = true;
             else
@@ -89,27 +73,44 @@ sealed class OptionsService : IOptionsService
 
     public void WriteUsage(TextWriter textWriter)
     {
+        /*-------------------------------------------------------------------------
+          Trace output options
+          0 | A: prints the states of the scanner automaton
+          1 | F: prints the First and Follow sets of all nonterminals
+          2 | G: prints the syntax graph of the productions
+          3 | I: traces the computation of the First sets
+          4 | J: prints the sets associated with ANYs and synchronisation sets
+          6 | S: prints the symbol table (terminals, nonterminals, pragmas)
+          7 | X: prints a cross reference list of all syntax symbols
+          8 | P: prints statistics about the Coco run
+
+          Trace output can be switched on by the pragma
+            $ { digit | letter }
+          in the attributed grammar or as a command-line option
+          -------------------------------------------------------------------------*/
+
         textWriter.WriteLine(
-            "Options:{0}"
-            + "  --namespace arg      Namespace name.{0}"
-            + "  --frames arg         Frame files directory.{0}"
-            + "  --trace arg          Trace string (see below).{0}"
-            + "  -o [ --output ] arg  Output directory.{0}"
-            + "  --lines              Emit lines.{0}"
-            + "{0}"
-            + "Valid characters in the trace string:{0}"
-            + "  A  trace automaton{0}"
-            + "  F  list first/follow sets{0}"
-            + "  G  print syntax graph{0}"
-            + "  I  trace computation of first sets{0}"
-            + "  J  list ANY and SYNC sets{0}"
-            + "  P  print statistics{0}"
-            + "  S  list symbol table{0}"
-            + "  X  list cross reference table{0}"
-            + "{0}"
-            + "Scanner.frame and Parser.frame files are needed in the ATG directory{0}"
-            + "or in a directory specified by the --frames option.",
-            Environment.NewLine);
+            """
+            Options:
+              --namespace arg      Namespace name.
+              --frames arg         Frame files directory.
+              --trace arg          Trace string (see below).
+              -o [ --output ] arg  Output directory.
+              --lines              Emit lines.
+
+            Valid characters in the trace string:
+              A  trace automaton
+              F  list first/follow sets
+              G  print syntax graph
+              I  trace computation of first sets
+              J  list ANY and SYNC sets
+              P  print statistics
+              S  list symbol table
+              X  list cross reference table
+            
+            Scanner.frame and Parser.frame files are needed in the ATG directory
+            or in a directory specified by the --frames option.
+            """);
     }
 }
 
--- Source/Gapotchenko.Turbo.CocoR/Program.cs
+++ Source/Gapotchenko.Turbo.CocoR/Program.cs
@@ -29,55 +29,47 @@ static class Program
     static void Run(IReadOnlyList<string> args)
     {
         Console.WriteLine("Turbo Coco/R 2022.1.1");
+        Console.WriteLine();
 
         var optionsService = new OptionsService(args);
 
         if (args.Count > 0 && optionsService.HasSourceFileName)
         {
-            Console.WriteLine();
-            try
+            string traceFilePath = Path.Combine(optionsService.OutputDirectoryName, "Trace.txt");
+
+            var scanner = new Scanner(optionsService.SourceFileName);
+            var parser = new Parser(scanner)
             {
-                string traceFilePath = Path.Combine(optionsService.OutputDirectoryName, "Trace.txt");
+                trace = new StreamWriter(new FileStream(traceFilePath, FileMode.Create))
+            };
 
-                var scanner = new Scanner(optionsService.SourceFileName);
-                var parser = new Parser(scanner)
-                {
-                    trace = new StreamWriter(new FileStream(traceFilePath, FileMode.Create))
-                };
+            parser.tab = new Tab(parser) { KeepOldFiles = optionsService.KeepOldFiles };
+            parser.dfa = new DFA(parser);
+            parser.pgen = new ParserGen(parser);
 
-                parser.tab = new Tab(parser) { KeepOldFiles = optionsService.KeepOldFiles };
-                parser.dfa = new DFA(parser);
-                parser.pgen = new ParserGen(parser);
+            parser.tab.srcName = optionsService.SourceFileName;
+            parser.tab.srcDir = optionsService.SourceDirectoryName;
+            parser.tab.nsName = optionsService.Namespace;
+            parser.tab.frameDir = optionsService.FramesDirectoryName;
+            parser.tab.outDir = optionsService.OutputDirectoryName;
+            parser.tab.emitLines = optionsService.EmitLines;
+            if (optionsService.Trace is not null and var trace)
+                parser.tab.SetTrace(trace);
 
-                parser.tab.srcName = optionsService.SourceFileName;
-                parser.tab.srcDir = optionsService.SourceDirectoryName;
-                parser.tab.nsName = optionsService.Namespace;
-                parser.tab.frameDir = optionsService.FramesDirectoryName;
-                parser.tab.outDir = optionsService.OutputDirectoryName;
-                parser.tab.emitLines = optionsService.EmitLines;
-                if (optionsService.Trace is not null and var trace)
-                    parser.tab.SetTrace(trace);
+            parser.Parse();
 
-                parser.Parse();
-
-                parser.trace.Close();
-                var f = new FileInfo(traceFilePath);
-                if (f.Length == 0)
-                    f.Delete();
-                else
-                    Console.WriteLine("Trace output has been written to \"{0}\" file.", traceFilePath);
-                Console.WriteLine("{0} errors detected.", parser.errors.count);
-                if (parser.errors.count != 0)
-                    throw new ProgramExitException(1);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("-- " + e.Message);
-            }
+            parser.trace.Close();
+            var f = new FileInfo(traceFilePath);
+            if (f.Length == 0)
+                f.Delete();
+            else
+                Console.WriteLine("Trace output has been written to \"{0}\" file.", traceFilePath);
+            Console.WriteLine("{0} errors detected.", parser.errors.count);
+            if (parser.errors.count != 0)
+                throw new ProgramExitException(1);
         }
         else
         {
-            Console.WriteLine();
             Console.WriteLine("Usage: turbo-coco grammar.atg [options]");
             Console.WriteLine();
             optionsService.WriteUsage(Console.Out);
