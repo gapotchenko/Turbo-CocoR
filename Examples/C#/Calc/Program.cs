9e7b3dc5fd2334bb4daf2e4556158d2c197ec9a6
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-11-25 03:09:27 +0100

    C# Calc sample in progress.

--- Examples/C#/Calc/Program.cs
+++ Examples/C#/Calc/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Calc;
+﻿using Calc.Grammar;
+using System.Text;
+
+namespace Calc;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        for (; ; )
+        {
+            Console.WriteLine("Enter the expression to calculate:");
+
+            string? line = Console.ReadLine();
+            if (string.IsNullOrEmpty(line))
+                break;
+
+            var parser = new Parser(new Scanner(new MemoryStream(Encoding.UTF8.GetBytes(line))));
+            parser.Parse();
+            Console.WriteLine(parser.Result);
+
+            Console.WriteLine();
+        }
     }
 }
