65d5909f8871b12d590e5bf5263e34ab1a52399f
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-12-07 15:07:05 +0100

    Build integration.

--- Source/Integration/Build/MSBuild/Gapotchenko.Turbo.CocoR.Build.MSBuild/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Gapotchenko.Turbo.CocoR.Build.MSBuild;
-
-public class Class1
-{
-
-}
---
+++ Source/Integration/Build/MSBuild/Gapotchenko.Turbo.CocoR.Build.MSBuild/TcrCompileGrammar.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace Gapotchenko.Turbo.CocoR.Build.MSBuild;
+
+/// <summary>
+/// Turbo Coco/R grammar compilation task for MSBuild.
+/// </summary>
+public sealed class TcrCompileGrammar : Task
+{
+    public override bool Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
