26a35b55583b13bdad8d5afaf8b9099766dc88a0
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-12-07 20:37:36 +0100

    Build integration.

--- Source/Integration/Build/MSBuild/Gapotchenko.Turbo.CocoR.Build.MSBuild.Tasks/TcrCompileGrammar.cs
+++ Source/Integration/Build/MSBuild/Gapotchenko.Turbo.CocoR.Build.MSBuild.Tasks/TcrCompileGrammar.cs
@@ -1,14 +1,67 @@
-﻿using Microsoft.Build.Utilities;
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.IO;
 
 namespace Gapotchenko.Turbo.CocoR.Build.MSBuild.Tasks;
 
 /// <summary>
 /// Turbo Coco/R grammar compilation task for MSBuild.
 /// </summary>
-public sealed class TcrCompileGrammar : Task
+public sealed class TcrCompileGrammar : ToolTask
 {
+    protected override string ToolName => "turbo-coco";
+
+    protected override string GenerateFullPathToTool() =>
+        Path.GetFullPath(
+            Path.Combine(
+                typeof(TcrCompileGrammar).Assembly.Location,
+                @"..\..\..\..",
+                ToolName));
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    string? m_Grammar;
+
+    [Required]
+    public string Grammar
+    {
+        get => m_Grammar ?? throw new InvalidOperationException("Grammar is not set.");
+        set => m_Grammar = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
     public override bool Execute()
     {
-        throw new System.NotImplementedException();
+        try
+        {
+            var grammarFilePath = Grammar;
+            var grammarFile = new FileInfo(grammarFilePath);
+
+            if (!grammarFile.Exists)
+            {
+                Log.LogError(null, "TCR0002", null, null, 0, 0, 0, 0, "Grammar file \"{0}\" does not exist.", grammarFilePath);
+                return false;
+            }
+
+            bool grammarIsEmpty = grammarFile.Length == 0;
+            if (!grammarIsEmpty)
+            {
+                // Use a text reader because the file can have a BOM but still be empty.
+                using var tr = grammarFile.OpenText();
+                grammarIsEmpty = tr.Read() == -1;
+            }
+
+            if (grammarIsEmpty)
+            {
+            }
+
+            Log.LogMessage(MessageImportance.High, "Hello from MSBuild task");
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.LogErrorFromException(e);
+            return false;
+        }
     }
 }
