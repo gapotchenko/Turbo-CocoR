3894b7e75823056d2e7b57929341f05f21555783
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-12-14 14:07:51 +0100

    MSBuild integration: changed tool layout.

--- Source/Integration/MSBuild/Gapotchenko.Turbo.CocoR.Integration.MSBuild/Package/tools/turbo-coco.cmd
+++ Source/Integration/MSBuild/Gapotchenko.Turbo.CocoR.Integration.MSBuild/Package/tools/turbo-coco.cmd
@@ -7,6 +7,6 @@ set SCRIPT_DIR=%~dp0.
 set DOTNET=dotnet
 if not "%DOTNET_ROOT%" == "" set DOTNET=%DOTNET_ROOT%\%DOTNET%
 
-"%DOTNET%" "%SCRIPT_DIR%\Gapotchenko.Turbo.CocoR.dll" %*
+"%DOTNET%" "%SCRIPT_DIR%\net7.0\any\Gapotchenko.Turbo.CocoR.dll" %*
 
 exit /b %ERRORLEVEL%
